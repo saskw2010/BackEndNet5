9b3d52bf2ee5d194d4e90a956dd5aae45aad79de
Mahmoud Saad <mahmoud.saad.abdelhamid@gmail.com>
2020-11-18 00:44:54 +0200

    sss

--- BackEnd.Web/Hubs/ChatHub.cs
+++ BackEnd.Web/Hubs/ChatHub.cs
@@ -11,28 +11,37 @@ namespace BackEnd.Web.Hubs
     
     public Task SendMessage1(string connectionId, string user, string message)               // Two parameters accepted
         {
-            return Clients.Client(connectionId).SendAsync("ReceiveOne", user, message);    // Note this 'ReceiveOne' 
+            return Clients.All.SendAsync("ReceiveOne", user, message);    // Note this 'ReceiveOne' 
         }
-
-    public chatUsers getConnectionUserList()
+    
+    public chatUsers getConnectionUserList(string userName, string email, string phoneNumber)
         {
-        chatUsers chatUsersList = chatUsers.Instance;
-        var user = new User();
+      chatUsers chatUsersList = chatUsers.Instance;
+      var user = new User();
+      if (!chatUsersList.Any(x => x.email == email)) {
+        user.userName = userName;
+        user.email = email;
+        user.phone = phoneNumber;
         user.connectionId = Context.ConnectionId;
-        chatUsersList.Add(user);
-          return chatUsersList;
-        }
+        chatUsersList.Add(user); 
+      }
+      return chatUsersList;
+    }
 
+ 
     public override async Task OnConnectedAsync() {
       await Clients.All.SendAsync("UserConnected",Context.ConnectionId);
       await base.OnConnectedAsync();
     }
+
 
     public override async Task OnDisconnectedAsync(Exception Ex)
     {
       await Clients.All.SendAsync("UserDisConnected", Context.ConnectionId);
       await base.OnDisconnectedAsync(Ex);
     }
+
+
 
   }
 }
