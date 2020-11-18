428967f4381c72e431a0a39571c86c77095cd916
Mahmoud Saad <mahmoud.saad.abdelhamid@gmail.com>
2020-11-18 14:21:47 +0200

    asd

--- BackEnd.Web/Hubs/ChatHub.cs
+++ BackEnd.Web/Hubs/ChatHub.cs
@@ -1,3 +1,4 @@
+using BackEnd.DAL.Context;
 using Microsoft.AspNetCore.SignalR;      // using this
 using System;
 using System.Collections.Generic;
@@ -8,38 +9,53 @@ namespace BackEnd.Web.Hubs
 {
   public class ChatHub : Hub      // inherit this
   {
-    
-    public Task SendMessage1(string connectionId, string user, string message)               // Two parameters accepted
+    private readonly BakEndContext DbContext;
+
+
+    public ChatHub(BakEndContext dbContext)
+    {
+      DbContext = dbContext;
+    }
+
+    public Task SendMessage1(string toconnectionId,string currentConnectionId, string user, string message)               // Two parameters accepted
         {
+            //return Clients.Clients(new List<string>() { toconnectionId, currentConnectionId }).SendAsync("ReceiveOne", user, message);    // Note this 'ReceiveOne' 
             return Clients.All.SendAsync("ReceiveOne", user, message);    // Note this 'ReceiveOne' 
         }
-    
+    chatUsers chatUsersList = chatUsers.Instance;
     public chatUsers getConnectionUserList(string userName, string email, string phoneNumber)
         {
       chatUsers chatUsersList = chatUsers.Instance;
       var user = new User();
-      if (!chatUsersList.Any(x => x.email == email)) {
-        user.userName = userName;
+      if (chatUsersList.Any(x => x.email == email))
+      {
+         chatUsersList.Remove(chatUsersList.SingleOrDefault(x => x.email == email));
+      }
+      user.userName = userName;
         user.email = email;
         user.phone = phoneNumber;
         user.connectionId = Context.ConnectionId;
-        chatUsersList.Add(user); 
-      }
+        chatUsersList.Add(user);
+      
       return chatUsersList;
     }
 
- 
-    public override async Task OnConnectedAsync() {
-      await Clients.All.SendAsync("UserConnected",Context.ConnectionId);
-      await base.OnConnectedAsync();
-    }
+   
 
 
-    public override async Task OnDisconnectedAsync(Exception Ex)
-    {
-      await Clients.All.SendAsync("UserDisConnected", Context.ConnectionId);
-      await base.OnDisconnectedAsync(Ex);
-    }
+    //public override async Task OnConnectedAsync()
+    //{
+    // await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+    //  await base.OnConnectedAsync();
+    //}
+
+
+    //public override async Task OnDisconnectedAsync(Exception Ex)
+    //{
+
+    //  await Clients.All.SendAsync("UserDisConnected", Context.ConnectionId);
+    //  await base.OnDisconnectedAsync(Ex);
+    //}
 
 
 
