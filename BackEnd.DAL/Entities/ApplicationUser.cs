96b190d9573b3d9f2bf12dff25a045847be7c671
Mahmoud Saad <mahmoud.saad.abdelhamid@gmail.com>
2020-12-16 09:56:45 +0200

    aasd

--- BackEnd.DAL/Entities/ApplicationUser.cs
+++ BackEnd.DAL/Entities/ApplicationUser.cs
@@ -14,11 +14,11 @@ namespace BackEnd.DAL.Entities
       public Boolean? EmailConfirmed { get; set; }
       public Boolean? IsApproved { get; set; }
       public Boolean? PhoneNumberConfirmed { get; set; }
-     public DateTime creationDate { get; set; }
-     public DateTime lastLoginDate { get; set; }
-     public DateTime lastActivityDate { get; set; }
-     public DateTime lastPasswordChangedDate { get; set; }
-     public DateTime lastLockedOutDate { get; set; }
+     public DateTime? creationDate { get; set; }
+     public DateTime? lastLoginDate { get; set; }
+     public DateTime? lastActivityDate { get; set; }
+     public DateTime? lastPasswordChangedDate { get; set; }
+     public DateTime? lastLockedOutDate { get; set; }
     public int? resetPasswordCode { get; set; }
       [ForeignKey("UserType")]
     public int? userTypeId { get; set; }
