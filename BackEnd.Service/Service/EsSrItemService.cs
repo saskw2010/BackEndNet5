f423b42503c8a62b04dd5c9bca628f4849894190
Mahmoud Saad <mahmoud.saad.abdelhamid@gmail.com>
2021-01-31 13:10:24 +0200

    asd

--- BackEnd.Service/Service/EsSrItemService.cs
+++ BackEnd.Service/Service/EsSrItemService.cs
@@ -1,4 +1,5 @@
 using BackEnd.BAL.Models;
+using BackEnd.DAL.Context;
 using BackEnd.Service.IService;
 using RealState.DAL.IBackEndContext;
 using System;
@@ -13,16 +14,17 @@ namespace BackEnd.Service.Service
         #region privateFilde
         private BAL.Interfaces.IUnitOfWork _unitOfWork;
         private AutoMapper.IMapper _mapper;
-        private IBackEndContext _db;
+        private BakEndContext _db;
         #endregion
 
         #region EsSrItemService
         public EsSrItemService(BAL.Interfaces.IUnitOfWork unitOfWork, 
             AutoMapper.IMapper mapper,
-            IBackEndContext db)
+            BakEndContext db)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _db = db;
         }
         #endregion
 
@@ -32,64 +34,7 @@ namespace BackEnd.Service.Service
             try
             {
                 List<object> list = new List<object>();
-                using (var cmd = _db.Database.GetDbConnection().CreateCommand())
-                {
-                    cmd.CommandText = "EsSr_Proc_GetActiveItems";
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    _db.Database.OpenConnection();
-                    using (var result = cmd.ExecuteReader())
-                    {
-                        if (result.HasRows)
-                        {
-                            list.Add(new 
-                            {
-                                ItemID = result["ItemID"]?.ToString(),
-                                CatgeoryID = result["CatgeoryID"]?.ToString(),
-                                CatgeoryNameAr = result["CatgeoryNameAr"]?.ToString(),
-                                CatgeoryNameEn = result["CatgeoryNameEn"]?.ToString(),
-                                CurrencyID = result["CurrencyID"]?.ToString(),
-                                CurrencyNameAr = result["CurrencyNameAr"]?.ToString(),
-                                CurrencyNameEn = result["CurrencyNameEn"]?.ToString(),
-                                CatgeoryPicStockId = result["CatgeoryPicStockId"]?.ToString(),
-                                CatgeoryId1 = result["CatgeoryId1"]?.ToString(),
-                                CatgeoryParentId = result["CatgeoryParentId"]?.ToString(),
-                                CatgeoryHasSubCatgeory = result["CatgeoryHasSubCatgeory"]?.ToString(),
-                                CatgeoryDescriptionEn = result["CatgeoryDescriptionEn"]?.ToString(),
-                                CatgeoryDescriptionAr = result["CatgeoryDescriptionAr"]?.ToString(),
-                                CatgeoryShowOrder = result["CatgeoryShowOrder"]?.ToString(),
-                                CatgeoryDiscount = result["CatgeoryDiscount"]?.ToString(),
-                                CatgeoryIsActive = result["CatgeoryIsActive"]?.ToString(),
-                                CatgeoryIsDelete = result["CatgeoryIsDelete"]?.ToString(),
-                                CatgeoryPicStockControllername = result["CatgeoryPicStockControllername"]?.ToString(),
-                                CatgeoryPicStockPictureFileName = result["CatgeoryPicStockPictureFileName"]?.ToString(),
-                                CatgeoryImageUrl = result["CatgeoryImageUrl"]?.ToString(),
-                                PicStockID = result["PicStockID"]?.ToString(),
-                                PicStockControllername = result["PicStockControllername"]?.ToString(),
-                                PicStockPictureFileName = result["PicStockPictureFileName"]?.ToString(),
-                                OrganisationID = result["OrganisationID"]?.ToString(),
-                                OrganisationNameAr = result["OrganisationNameAr"]?.ToString(),
-                                OrganisationNameEn = result["OrganisationNameEn"]?.ToString(),
-                                NameAr = result["NameAr"]?.ToString(),
-                                NameEn = result["NameEn"]?.ToString(),
-                                DescriptionEn = result["DescriptionEn"]?.ToString(),
-                                DescriptionAr = result["DescriptionAr"]?.ToString(),
-                                ShowOrder = result["ShowOrder"]?.ToString(),
-                                Discount = result["Discount"]?.ToString(),
-                                CreatedHour = result["CreatedHour"]?.ToString(),
-                                EditHour = result["EditHour"]?.ToString(),
-                                IsActive = result["IsActive"]?.ToString(),
-                                Notes = result["Notes"]?.ToString(),
-                                CreatedBy = result["CreatedBy"]?.ToString(),
-                                CreatedOn = result["CreatedOn"]?.ToString(),
-                                ModifiedBy = result["ModifiedBy"]?.ToString(),
-                                ModifiedOn = result["ModifiedOn"]?.ToString(),
-                                IsDelete = result["IsDelete"]?.ToString(),
-                                Price = result["Price"]?.ToString(),
-                                IsPreview = result["IsPreview"]?.ToString(),
-                            });
-                        }
-                    }
-                }
+               list = _db.GetEsSr_Proc_GetActiveItems();
                 return new Result
                 {
                     success = true,
